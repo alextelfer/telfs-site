4a8198e3d5e49d51bb2938075ec6310844f20ccf
alextelfer <alextelfer96@gmail.com>
2025-06-30 12:03:48 -0600

    Update supabaseClient.js
    minor changes

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'process.env.SUPABASE_URL'; // replace with your URL
-const supabaseAnonKey = 'process.env.SUPABASE_KEY'; // replace with your key
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
