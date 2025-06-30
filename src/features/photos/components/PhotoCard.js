a21e00ba84e5e2ce9926565d69610eb2b5657cfa
alextelfer <alextelfer96@gmail.com>
2025-06-30 10:28:41 -0600

    Update PhotoCard.js
    fixing lint error

--- src/features/photos/components/PhotoCard.js
+++ src/features/photos/components/PhotoCard.js
@@ -3,7 +3,7 @@ import React from 'react';
 const PhotoCard = ({ src }) => {
   return (
     <div>
-      <img src={src} alt="family photo" style={{ width: 150, height: 150, objectFit: 'cover' }} />
+      <img src={src} alt="gang being gang" style={{ width: 150, height: 150, objectFit: 'cover' }} />
     </div>
   );
 };
